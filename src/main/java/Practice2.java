45d48cb76c2f5fe59e4079e31ca5d1ba6ffc4168
Viner108 <andrukovichl@mail.ru>
2023-03-24 15:00:46 +0300

    Practice2 idea commit

--- src/main/java/Practice2.java
+++ src/main/java/Practice2.java
@@ -1,5 +1,12 @@
 public class Practice2 {
+    /**
+     * Идея
+     * Программа возвращет новый массив без повторяющихся значений.
+     * @param args
+     */
     public static void main(String[] args) {
+        int[] lines = new int[]{1, 2, 2, 3};
 
     }
 }
+
