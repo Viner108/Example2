a039795c4c319db70c8046bdffdae961e4c71f84
Viner108 <andrukovichl@mail.ru>
2023-03-24 14:40:18 +0300

    initial commit

---
+++ src/main/java/org/example/Practice1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Practice1 {
+    /**
+     * Идея
+     * Программа находит число в массиве и говорит его индекс
+     */
+    public static void main(String[] args) {
+        int[] lines = new int[]{1, 2, 3, 4};
+        int t = 4;
+
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i] == t) {
+                System.out.println("Индекс числа в массив равен " + i);
+                break;
+            }
+            }
+        }
+    }
+
